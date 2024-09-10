0881f0762c9928317a3e2917ae7d35416302139c
ElbeiglTV <gael.taborda@davinci.edu.ar>
2024-09-10 16:16:25 -0300

    Pakage

---
+++ Assets/Script/Stand.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stand : MonoBehaviour
+{
+   // list of Items for this stand (Item)
+   // list of Visual objects for this stand (GameObject)
+
+   
+
+
+}
